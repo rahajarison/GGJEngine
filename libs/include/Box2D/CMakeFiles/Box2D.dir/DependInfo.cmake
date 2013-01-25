0386e1adf921a42fe1707f4342d9ee1653600022
rahajarison <rahajarison.johnny+work@gmail.com>
2013-01-25 22:34:14 +0100

    Ajouts des nouvelles sources et des includes Box2D

---
+++ libs/include/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/Shapes/b2ChainShape.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2ChainShape.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/Shapes/b2CircleShape.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2CircleShape.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/Shapes/b2EdgeShape.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2EdgeShape.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/Shapes/b2PolygonShape.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2PolygonShape.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2BroadPhase.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2BroadPhase.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2CollideCircle.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideCircle.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2CollideEdge.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideEdge.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2CollidePolygon.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollidePolygon.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2Collision.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2Collision.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2Distance.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2Distance.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2DynamicTree.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2DynamicTree.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Collision/b2TimeOfImpact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Collision/b2TimeOfImpact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Common/b2BlockAllocator.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Common/b2BlockAllocator.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Common/b2Draw.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Common/b2Draw.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Common/b2Math.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Common/b2Math.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Common/b2Settings.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Common/b2Settings.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Common/b2StackAllocator.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Common/b2StackAllocator.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Common/b2Timer.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Common/b2Timer.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2ChainAndCircleContact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndCircleContact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2ChainAndPolygonContact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndPolygonContact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2CircleContact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2CircleContact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2Contact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2Contact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2ContactSolver.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ContactSolver.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2EdgeAndCircleContact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndCircleContact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2PolygonAndCircleContact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonAndCircleContact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Contacts/b2PolygonContact.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonContact.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2DistanceJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2DistanceJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2FrictionJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2FrictionJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2GearJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2GearJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2Joint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2Joint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2MouseJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2MouseJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2PrismaticJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PrismaticJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2PulleyJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PulleyJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2RevoluteJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RevoluteJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2RopeJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RopeJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2WeldJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WeldJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/Joints/b2WheelJoint.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WheelJoint.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/b2Body.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Body.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/b2ContactManager.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2ContactManager.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/b2Fixture.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Fixture.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/b2Island.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Island.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/b2World.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2World.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Dynamics/b2WorldCallbacks.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2WorldCallbacks.cpp.obj"
+  "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/Rope/b2Rope.cpp" "C:/Users/Seven/Documents/GitHub/GGJEngine/Box2D_v2.2.1/Box2D/CMakeFiles/Box2D.dir/Rope/b2Rope.cpp.obj"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "Box2D/.."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ sources/includes/Game.hpp
@@ -0,0 +1,6 @@
+#ifndef				GAME_H_
+# define			GAME_H_
+
+
+
+#endif /* !GAME_H_ */
