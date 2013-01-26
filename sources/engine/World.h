d8f4e84dde38b660569e8c550a071b9f7a5be6c2
Maurogik <gwenn.aubert@gmail.com>
2013-01-26 20:06:30 +0100

    maj world with new pattern

--- sources/engine/World.h
+++ sources/engine/World.h
@@ -8,6 +8,8 @@
 #include "PatternFall1.h"
 #include "PatternFall2.h"
 #include "PatternFall3.h"
+#include "PatternFall4.h"
+#include "PatternFall5.h"
 
 using namespace std;
 
@@ -27,9 +29,13 @@ class World
             patternList.push_back(new PatternFall1(0, 0));
             patternList.push_back(new PatternFall2(40, 0));
             patternList.push_back(new PatternFall3(80, 0));
+            patternList.push_back(new PatternFall4(80, 40));
+            patternList.push_back(new PatternFall5(80, 80));
             patternList[0]->create();
             patternList[1]->create();
             patternList[2]->create();
+            patternList[3]->create();
+            patternList[4]->create();
         }
         ~World() { }
 };
