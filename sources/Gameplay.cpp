eb998550884e99d0f06b58c1588e67928a1df32b
rahajarison <rahajarison.johnny+work@gmail.com>
2013-01-26 14:32:05 +0100

    Compilation pour Mac

---
+++ sources/Gameplay.cpp
@@ -0,0 +1,29 @@
+#include	<iostream>
+#include	<GGJContext.hpp>
+#include	"engine/World.h"
+#include	"Game.hpp"
+#include	"engine/Character.h"
+#include	"ObjDebug.hpp"
+#include	"Cell.hpp"
+#include	"Gameplay.hpp"
+
+void		registerImpulse(void)
+{
+	sf::Event event;
+	GGJ::Context&	context = GGJ::Context::getSingleton();
+
+	event.Type = sf::Event::KeyPressed;
+	event.Key.Code = sf::Key::I;
+	GGJ::registeringCallback callback(&OnImpulseEvent, &context);
+	context.registerOnEvent(event, callback);	
+}
+void		registerDivide(void)
+{
+	sf::Event event;
+	GGJ::Context&	context = GGJ::Context::getSingleton();
+
+	event.Type = sf::Event::KeyPressed;
+	event.Key.Code = sf::Key::D;
+	GGJ::registeringCallback callback(&OnDivideEvent, &context);
+	context.registerOnEvent(event, callback);
+}
