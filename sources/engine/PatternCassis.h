33e88a271fe00676e4e84f1d3ae7b22085ba4e73
Maurogik <gwenn.aubert@gmail.com>
2013-01-27 01:40:31 +0100

    m

---
+++ sources/engine/PatternCassis.h
@@ -0,0 +1,45 @@
+#ifndef PATTERN_CASSIS_H
+#define PATTERN_CASSIS_H
+
+#include "Pattern.h"
+
+class PatternCassis : Pattern
+{
+public:
+    PatternCassis(int x) : Pattern(x){}
+    void create(){
+        b2BodyDef myBodyDef;
+
+        //shape definition
+        b2PolygonShape polygonShape;
+
+      
+        //fixture definition
+        b2FixtureDef myFixtureDef;
+        myFixtureDef.shape = &polygonShape;
+        myFixtureDef.density = 1;
+
+        myBodyDef.type = b2_staticBody;
+        myBodyDef.position.Set(x+512,0);
+        b2Body* staticBody = World::m_world->CreateBody(&myBodyDef);
+        polygonShape.SetAsBox( 1, 512);//ground
+        staticBody->CreateFixture(&myFixtureDef);
+
+        myBodyDef.position.Set(x+512,800);
+        staticBody = World::m_world->CreateBody(&myBodyDef);
+        polygonShape.SetAsBox( 1, 512;//ground
+        staticBody->CreateFixture(&myFixtureDef);
+        int radius=100;
+        b2CircleShape circleShape;
+        myFixtureDef.shape=circleShape;
+        circleShape.m_p.Set(0, 0);          //position, relative to body position
+        circleShape.m_radius = radius;
+        myBodyDef.position.Set(x+512, radius*9/10);
+        staticBody = World::m_world->CreateBody(&myBodyDef);
+        staticBody->CreateFixture(&myFixtureDef);
+
+
+    }
+    ~PatternVide();
+};
+
